1bda3c91f121303b6a6dcdf376f8cc9ab0f96247
黐麟 <tirin.script.modder@gmail.com>
2017-09-10 22:01:16 +0900

    Update OpenGL.js

--- OpenGL.js
+++ OpenGL.js
@@ -4,23 +4,23 @@ var Runnable=java.lang.Runnable;
 var context=MainActivity.currentMainActivity.get();
 
 function newLevel(){
-	context.runOnUiThread(new Runnable({
-		run:function(){
-			try{
-				var view=new GLSurfaceView(context);
-				view.setRenderer(new GLSurfaceView.Renderer({
-					onSurfaceCreated:function(gl,config){
-					},
-					onSurfaceChanged:function(gl,width,height){
-					},
-					onDrawFrame:function(gl){
-					}
-				}));
-				context.setContentView(view);
-			}
-			catch(error){
-				throw new Error(error);
-			}
-		}
-	}));
+  context.runOnUiThread(new Runnable({
+    run:function(){
+      try{
+        var view=new GLSurfaceView(context);
+        view.setRenderer(new GLSurfaceView.Renderer({
+          onSurfaceCreated:function(gl,config){
+          },
+          onSurfaceChanged:function(gl,width,height){
+          },
+          onDrawFrame:function(gl){
+          }
+        }));
+        context.setContentView(view);
+      }
+      catch(error){
+        throw new Error(error);
+      }
+    }
+  }));
 }
